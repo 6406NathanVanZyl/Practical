b886ef14a9867cdcb606709219bc00fab9abc29e
Nathan Van Zyl <6406@ctucareer.co.za>
2021-08-20 09:01:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("How are you today?");
+            Console.ReadLine();
+            Console.WriteLine("Im sorry to hear that hope your day gets better.");
+        }
+    }
+}
